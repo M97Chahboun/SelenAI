20f338344271149fd6ddaefb65c8b265bcae8a0a
Mohammed <m97.chahboun@gmail.com>
2025-12-17 23:55:41 +0100

    renamed entrypoint file

--- browser_agent.py
+++ browser_agent.py
@@ -10,10 +10,9 @@ import time
 from typing import List, Dict, Any, Optional
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException
 import google.generativeai as genai
 
 
